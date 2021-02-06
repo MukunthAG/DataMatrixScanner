868b8dea3dd9566800117cd2d4c95ceffa1d5878
MukunthAG <mukunthag@gmail.com>
2021-02-06 16:09:19 +0530

    Raw working edition of GS1 barcode scanner, if the barcode is perfect

--- ImgProcessingPython/barcodeScan.py
+++ ImgProcessingPython/barcodeScan.py
@@ -22,7 +22,6 @@ def getLineInfo(imgPath, rowToScan):
         B = [0, 0, 0] 
         W = [255, 255, 255]
         underScan = W 
-        #plt.figure(1)
         plt.subplot(1, 2, 1)
         plt.title("Working Image")
         for i in range(width):
@@ -59,7 +58,57 @@ def getLineInfo(imgPath, rowToScan):
 
     return lineInfo
     
-
 lineInfo = getLineInfo('barImgs/bar6.jpg', 75)
+refCodes = { 
+    '0' : [0, 0, 0, 1, 1, 0, 1],
+    '1' : [0, 0, 1, 1, 0, 0, 1],
+    '2' : [0, 0, 1, 0, 0, 1, 1],
+    '3' : [0, 1, 1, 1, 1, 0, 1],
+    '4' : [0, 1, 0, 0, 0, 1, 1],
+    '5' : [0, 1, 1, 0, 0, 0, 1],
+    '6' : [0, 1, 0, 1, 1, 1, 1],
+    '7' : [0, 1, 1, 1, 0, 1, 1],
+    '8' : [0, 1, 1, 0, 1, 1, 1],
+    '9' : [0, 0, 0, 1, 0, 1, 1]
+}
+def getCode(lines):
+    unitLength = lines[0][1]
+    binaryCode = []
+    for i, line in enumerate(lines):
+        for count in range(int(line[1]/unitLength)):
+            if (line[0] == 'B'):
+                binaryCode.append(0)
+            if (line[0] == 'W'):
+                binaryCode.append(1)
+    del binaryCode[0:3]; del binaryCode[42:47]; del binaryCode[-3:]
+    pp.pprint(binaryCode)
+    print(len(binaryCode))
+    binaryCode = np.array_split(binaryCode, 2)
+    leftParity = np.array_split(binaryCode[0], 6)
+    rightParity = np.array_split(binaryCode[1], 6)
+    pp.pprint(leftParity)
+    pp.pprint(rightParity)
+    decimalCode = []
 
+    def inversion(x):
+        def complement(n):
+            if (n == 0): 
+                return 1
+            else: 
+                return 0
+        return list(map(complement, x))
+     
+    for refCode in leftParity:
+        for key, value in refCodes.items():
+            if (np.array_equal(refCode, inversion(value))):
+                decimalCode.append(key)
+                break
+
+    for refCode in rightParity:
+        for key, value in refCodes.items():
+            if (np.array_equal(refCode, value)):
+                decimalCode.append(key)
+                break
+    pp.pprint(decimalCode)
+getCode(lineInfo)
 plt.show()
